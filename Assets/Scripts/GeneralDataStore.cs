02fb3bde5c1af4ff06ce3a77448f662196d3629e
sid68v <sidharthv.cusat@gmail.com>
2020-10-11 00:37:31 +0530

    Added assets. created level 1.

---
+++ Assets/Scripts/GeneralDataStore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class GeneralDataStore:MonoBehaviour
+    {
+
+
+    }
+public struct TAGS
+{
+    public const string Enemy = "enemy";
+}
+
---
+++ Assets/Scripts/TowerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerController : MonoBehaviour
+{
+
+    [SerializeField] Transform turret;
+    [HideInInspector]
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag(TAGS.Enemy).transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        turret.LookAt(target);
+    }
+}
